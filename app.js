85f39015204f00ee10b1b23f080303565841e8d8
Duong Thien Chi <thienchidtc@gmail.com>
2024-11-04 12:10:09 +0700

    register

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 db.connect();
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 app.use(logger("dev"));
 app.use(express.json());
