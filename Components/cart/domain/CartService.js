e3e8b1bd02377b2d800c97b21654f2972dedb34c
Sciderus <huydska11@gmail.com>
2024-12-07 15:19:57 +0700

    fix error about Cart Service

--- Components/cart/domain/CartService.js
+++ Components/cart/domain/CartService.js
@@ -2,26 +2,26 @@ const CartModel = require("../data-access/CartModel");
 
 const CartService = {
     GetCartByUserId: async (userId) => {
-        const cart = await Cart.GetCartByUserId(userId);
+        const cart = await CartModel.GetCartByUserId(userId);
         await cart.FetchAllCourses();
         return cart;
     },
 
     RemoveCourse: async (userId, courseId) => {
-        const cart = await Cart.GetCartByUserId(userId);
+        const cart = await CartModel.GetCartByUserId(userId);
         await cart.RemoveCourse(courseId);
         await cart.save();
     },
 
     syncCart: async (userId, cart) => {
-        const carts = await Cart.GetCartByUserId(userId);
+        const carts = await CartModel.GetCartByUserId(userId);
         if (carts.items.length === 0) {
             carts.items = cart;
             await carts.save();
             return;
         }
 
-        console.log("Received cart:", cart.length);
+        console.log("Received CartModel:", cart.length);
 
         cart.forEach((item) => {
             const existingItem = carts.items.find((i) => i._id == item._id);
@@ -30,9 +30,9 @@ const CartService = {
             }
         });
 
-        console.log("Updated cart:", carts.items.length);
+        console.log("Updated CartModel:", carts.items.length);
         await carts.save();
-    }
-}
+    },
+};
 
 module.exports = CartService;
