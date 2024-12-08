cd0eaa8897349333c2b3c5b00d94af4f3671b4ec
Sciderus <huydska11@gmail.com>
2024-12-08 14:27:11 +0700

    remove ajaxCourse

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const cartRoutes = require("./Components/cart/api/cart");
 
 // AJAX API
 const validate = require("./Components/validate/api/validate");
-const AjaxCourseRouter = require("./Components/course/api/AjaxCourse");
 const Payment = require("./Components/payment/data-access/PayModel");
 const Cart = require("./Components/cart/data-access/CartModel");
 const app = express();
@@ -125,7 +124,7 @@ app.use("/", dashboardRoutes);
 app.use("/courses", coursesRouter);
 app.use("/paycourses", paymentRouter);
 app.use("/cart", cartRoutes);
-app.use("/courses", AjaxCourseRouter);
+//app.use("/courses", AjaxCourseRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
