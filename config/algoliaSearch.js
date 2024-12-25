4d109a285709d90b144a1abce13af1fecc9156ee
Sciderus <huydska11@gmail.com>
2024-12-25 09:51:17 +0700

    integrate algoliasearch

---
+++ config/algoliaSearch.js
@@ -0,0 +1,5 @@
+const { searchClient } = require('algoliasearch');  // Access searchClient
+
+const client = searchClient('WAB3KVHFN2', '8d823068e91a2d9c9a9ee2dcc71f7463');
+
+module.exports = client;
