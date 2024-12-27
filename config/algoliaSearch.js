11dda407539bc6f55d3351649d1fa9b7755af4ce
Sciderus <huydska11@gmail.com>
2024-12-27 15:20:28 +0700

    install algolia 4.22.1 instead of 5.18.0

--- config/algoliaSearch.js
+++ config/algoliaSearch.js
@@ -1,7 +1,12 @@
-const { searchClient } = require('algoliasearch');
+const algoliasearch = require("algoliasearch");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 
-const client = searchClient(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
+const client = algoliasearch(
+    process.env.ALGOLIA_APP_ID,
+    process.env.ALGOLIA_API_KEY
+);
 
-module.exports = client;
+const index = client.initIndex("course");
+
+module.exports = index;
