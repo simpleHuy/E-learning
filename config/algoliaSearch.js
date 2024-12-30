c002c95cb7d834c633b088fb5a28e4db10c160f0
Sciderus <huydska11@gmail.com>
2024-12-30 17:05:41 +0700

    upgrade algoliasearch pakage to 5.18.0

--- config/algoliaSearch.js
+++ config/algoliaSearch.js
@@ -1,12 +1,7 @@
-const algoliasearch = require("algoliasearch");
+const { searchClient } = require('algoliasearch');
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 
-const client = algoliasearch(
-    process.env.ALGOLIA_APP_ID,
-    process.env.ALGOLIA_API_KEY
-);
+const client = searchClient(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
 
-const index = client.initIndex("course");
-
-module.exports = index;
+module.exports = client;
